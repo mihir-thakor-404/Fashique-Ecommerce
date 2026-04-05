9eb9794c154864e0d1b7d867f6dfd8ca90c2b134
mihir-thakor-404 <mihirthakor204@gmail.com>
2026-04-05 17:22:57 +0530

    update Fix configs Error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',           // ← Yeh zaroori hai static export ke liye
-  basePath: '/Fashique-Ecommerce',   // ← Tumhare repo ka exact naam (case sensitive)
-  assetPrefix: '/Fashique-Ecommerce', // ← Images aur assets ke liye
-  images: {
-    unoptimized: true,        // ← GitHub Pages pe next/image ke liye mandatory
-  },
+  // output: 'export',           // ← Yeh zaroori hai static export ke liye
+  // basePath: '/Fashique-Ecommerce',   // ← Tumhare repo ka exact naam (case sensitive)
+  // assetPrefix: '/Fashique-Ecommerce', // ← Images aur assets ke liye
+  // images: {
+  //   unoptimized: true,        // ← GitHub Pages pe next/image ke liye mandatory
+  // },
   trailingSlash: true,        // ← Common issue avoid karta hai
 };
 
